497d323bf39363290990625156a2d890fa14e9a0
bthank <bthankachan@yahoo.com>
2021-07-31 18:35:43 -0400

    Initial commit

---
+++ springmvc.gaming.demo/src/main/java/com/binu/gaming/demo/Application.java
@@ -0,0 +1,17 @@
+package com.binu.gaming.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	
+
+	
+}
